9bdc51edb8b60973c681ffda759736f19c160086
Isaac Sales <isaacrodrigues1516@gmail.com>
2025-07-15 17:42:10 -0300

    fix bugs

--- app/middlewares/error_handler.ts
+++ app/middlewares/error_handler.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from "express";
-import { ApiError } from "../utils/apiError";
 
 export function errorHandlerMiddleware(
-    err: any,
-    req: Request,
-    res: Response,
-    next: NextFunction
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) {
-    console.error(err);
-    if (err instanceof ApiError){
-        const status = res.status(err.status)
-        return status.json({ sucess: false, message: err.message });
-    }
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Erro interno no servidor";
+
+  if (req.headers.accept?.includes("application/json")) {
+    res.status(statusCode).json({ error: message });
+  } else {
+    res.status(statusCode).render("error", { message });
+  }
 }
--- start/server.ts
+++ start/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import path from 'path';
 import { engine } from 'express-handlebars';
+import { errorHandlerMiddleware } from '../app/middlewares/error_handler';
 import routes from '../routes/web';
 import methodOverride from 'method-override';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -18,6 +22,16 @@ app.set('views', path.resolve(__dirname, '..', 'resources', 'views'));
 app.use(express.static(path.resolve(__dirname, '..', 'public')))
 
 routes(app);
+
+// if no port is found, error 404 is returned (não me esquecer dessa budega)
+app.use((req, res) => {
+  res.status(404).render('error', {
+    status: 404,
+    message: 'Página não encontrada',
+  });
+});
+
+app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
